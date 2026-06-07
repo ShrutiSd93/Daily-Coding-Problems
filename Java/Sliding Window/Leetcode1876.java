79247c2805362fea04744be187362faaf8901a4e
ShrutiSd93 <shruti797240@gmail.com>
2026-06-07 23:46:39 +0530

    Leetcode problem number 1836 which is a substring of size three with distinct characters in both java and python

---
+++ Java/Sliding Window/Leetcode1876.java
@@ -0,0 +1,51 @@
+// 1876. Substrings of Size Three with Distinct Characters
+
+// A string is good if there are no repeated characters.
+// Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+// Note that if there are multiple occurrences of the same substring, 
+// every occurrence should be counted.
+// A substring is a contiguous sequence of characters in a string.
+
+// Example 1:
+
+// Input: s = "xyzzaz"
+// Output: 1
+// Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz". 
+// The only good substring of length 3 is "xyz".
+// Example 2:
+
+// Input: s = "aababcabc"
+// Output: 4
+// Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+// The good substrings are "abc", "bca", "cab", and "abc".
+ 
+// Constraints:
+
+// 1 <= s.length <= 100
+// s​​​​​​ consists of lowercase English letters.
+
+
+
+
+
+
+
+class Leetcode1876{
+    public static void main(String[] args){
+
+        String s="xyzzaz";
+        System.out.println(countGoodStrings(s));
+    }
+
+    public static int countGoodSubstrings(String s){
+        int count=0;
+
+        for(int i=0; i<s.length()-2;i++){
+            if(s.charAt(i) != s.charAt(i+1) && s.charAt(i+1) != s.charAt(i+2) && s.charAt(i) != s.charAt(i+2)){
+                count++;;
+            }
+        } 
+
+        return count;
+    }
+}
